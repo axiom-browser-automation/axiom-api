3b17606c37a358e9322702511bac306c492af9ed
Simon Williams <simon.williams@thegreatweb.co>
2025-10-23 12:48:53 +0100

    Completed skeleton of working API

--- src/axiom-api.js
+++ src/axiom-api.js
@@ -3,6 +3,7 @@ const ENDPOINT = 'https://lar-simon.axiom.ai'
 export class AxiomApi {
 
     constructor(token) {
+        this.cdpLink = ''
         this.token = token
     }
 
@@ -16,10 +17,203 @@ export class AxiomApi {
             body: JSON.stringify([])
         });
         const content = await rawResponse.json();
-
-        console.log(content);
-        
+        this.cdpLink = content.endpoint
         return content.endpoint
+    }
+
+    async browserClose(cdpLink = '') {
+        if (cdpLink) {
+            cdpLink = this.cdpLink
+        }
+        const rawResponse = await fetch(ENDPOINT + '/api/v5/browser/close', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({cdpLink})
+        });
+        const content = await rawResponse.json();        
+        return content.message
+    }
+
+    // TODO: Probably should be mvoed to a non-user facing component
+    async step(mode, method, params, cdpLink = '') {
+        const rawResponse = await fetch(ENDPOINT + '/api/v5/step', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({mode, method, params, cdpLink})
+        });
+        const content = await rawResponse.json();
+        if (content.message) {
+            return content.message
+        } else {
+            return content
+        }
+    }
+
+    async scrape(url, selector, pager, max_results, settings) {
+        return this.step(
+            'browser',
+            'AxiomApiSmartScrapeV440',
+            [url, selector, pager, max_results, settings],
+            this.cdpLink
+        )
+    }
+
+    async integrateAI(aiOptions) {
+        return this.step(
+            'browser',
+            'AxiomApiAiGeneric',
+            [aiOptions],
+            this.cdpLink
+        )
+    }
+
+    async datePicker(selectMonth, selectMonthChangeButton, changeMonthTo, changeDayOfMonthTo) {
+        return this.step(
+            'driver',
+            'driver.datePicker',
+            [selectMonth, selectMonthChangeButton, changeMonthTo, changeDayOfMonthTo],
+            this.cdpLink
+        )
+    }
+
+    async click(select, leftClickRightClick, optionalClick) {
+        return this.step(
+            'driver',
+            'driver.clickV3130',
+            [select, leftClickRightClick, optionalClick],
+            this.cdpLink
+        )
+    }
+
+    async clickEngagementButton(select, setValueToCheck) {
+        return this.step(
+            'driver',
+            'driver.clickEngagementButton',
+            [select, setValueToCheck],
+            this.cdpLink
+        )
+    }
+
+    async clickMultiple(select, leftClickRightClick, maxClicks) {
+        return this.step(
+            'driver',
+            'driver.multiClickV3170',
+            [select, leftClickRightClick, maxClicks],
+            this.cdpLink
+        )
+    }
+
+    async getClipboardContents() {
+        return this.step(
+            'driver',
+            'driver.readClipboardContents',
+            [],
+            this.cdpLink
+        )
+    }
+
+    async enterText(selectTextField, text, delay, appendExisting, customLineBreak, optionalText) {
+        return this.step(
+            'driver',
+            'driver.enterTextV4500',
+            [selectTextField, text, delay, appendExisting, customLineBreak, optionalText],
+            this.cdpLink
+        )
+    }
+
+    async goto(url, doNotShareLocalstorage, openInNewTab) {
+        return this.step(
+            'driver',
+            {driver: "driver.gotoV4070"},
+            [url, null, doNotShareLocalstorage, openInNewTab]
+        )
+    }
+
+    async pressKeys(key, delimiter, delay) {
+        return this.step(
+            'driver',
+            'driver.keydownV3120',
+            [key, null, delimiter, delay],
+            "interact"
+        )
+    }
+
+    async clickAndDrag(startCoordinates, endCoordinates) {
+        return this.step(
+            'driver',
+            'driver.mouseClickDragV0300',
+            [startCoordinates, endCoordinates],
+            this.cdpLink
+        )
+    }
+
+    async scrapeMetadata(metadata) {
+        return this.step(
+            'driver',
+            { driver: "driver.scrapeMetadata" },
+            [metadata],
+            this.cdpLink
+        )
+    }
+
+    async selectList(select, text) {
+        return this.step(
+            'driver',
+            {driver: "driver.selectList"},
+            [select, text],
+            this.cdpLink
+        )
+    }
+
+    async solveCaptcha(apiKey) {
+        return this.step(
+            'driver',
+            'driver.solveCaptchaV450',
+            [null, null, apiKey],
+            this.cdpLink
+        )
+    }
+
+    async switchBrowserTab(selectTab) {
+        return this.step(
+            'driver',
+            'driver.switchBrowserTab',
+            [selectTab],
+            this.cdpLink
+        )
+    }
+
+    async wait(timeOptions) {
+        return this.step(
+            'driver',
+            'driver.waitV4000',
+            [timeOptions],
+            this.cdpLink
+        )
+    }
+
+    async hover(select) {
+        return this.step(
+            'driver',
+            'driver.hover',
+            [select],
+            this.cdpLink
+        )
+    }
+
+    async restartBrowser() {
+        return this.step(
+            'browser',
+            'AxiomApiRestartBrowser',
+            [],
+            this.cdpLink
+        )
     }
 
 }
--- test/base.test.js
+++ test/base.test.js
@@ -1,24 +1,145 @@
 import { AxiomApi } from '../src/axiom-api.js';
 import nock from 'nock';
 
-const scope = nock('https://lar-simon.axiom.ai')
+nock('https://lar-simon.axiom.ai')
     .post('/api/v5/browser/open')
     .reply(200, {
         endpoint: 'ws://test-cdp-link'
     })
+    .post('/api/v5/browser/close')
+    .reply(200, {
+        message: 'Browser session closed successfully.'
+    })
+
+const axiomApi = new AxiomApi('test-token')
 
 describe('Basic library tests', () => {
 
+    beforeEach(() => {
+
+    })
+
     // Basic boostrap test
     test('should bootstrap AxiomApi class', () => {
-        const axiomApi = new AxiomApi('test-token');
         expect(axiomApi.token).toBe("test-token");
     });
 
-    test('should open a browser', async () => {
-        const axiomApi = new AxiomApi('test-token');
+    test('should open and close a browser', async () => {
         const cdpLink = await axiomApi.browserOpen();
         expect(cdpLink).toBe('ws://test-cdp-link')
+        const message = await axiomApi.browserClose(cdpLink)
+        expect(message).toBe('Browser session closed successfully.')
+    })
+
+    test('should trigger a step', async () => {
+        nock('https://lar-simon.axiom.ai')
+            .post('/api/v5/step')
+            .reply(200, {
+                message: 'Step command executed successfully.'
+            })
+        const axiomApi = new AxiomApi('test-token')
+        const message = await axiomApi.step('mode', 'method', [])
+        expect(message).toBe('Step command executed successfully.')
+    })
+
+    test('should scrape data', async () => {
+        nock('https://lar-simon.axiom.ai')
+            .post('/api/v5/step', {
+                mode: 'browser',
+                method: 'AxiomApiSmartScrapeV440',
+                params: ['https://test', 'test', 'test', 20, {}],
+                cdpLink: ''
+            })
+            .reply(200, [
+                ['A', 'B', 'C']
+            ])
+        const results = await axiomApi.scrape('https://test', 'test', 'test', 20, {})
+        expect(results).toStrictEqual([['A', 'B', 'C']])
+    })
+
+    test('should retrieve response from llm', async() => {
+        const results = await axiomApi.integrateAI({})
+        expect(true).toBe(false)
+    })
+
+    test('should interact with a date picker', async() => {
+        const results = await axiomApi.datePicker('October', '.button', 'November', '13')
+        expect(true).toBe(false)
+    })
+
+    test('should click', async() => {
+        const results = await axiomApi.click('.button', false, false)
+        expect(true).toBe(false)
+    })
+
+    test('should click engagement button', async() => {
+        const results = await axiomApi.clickEngagementButton('.button', 'Liked')
+        expect(true).toBe(false)
+    })
+
+    test('should click multiple', async() => {
+        const results = await axiomApi.clickMultiple('.button', false, 0)
+        expect(true).toBe(false)
+    })
+
+    test('should get clipboard contents', async() => {
+        const results = await axiomApi.getClipboardContents()
+        expect(true).toBe(false)
+    })
+
+    test('should enter text', async() => {
+        const results = await axiomApi.enterText('.input', 'The Text', 0, false, null, false)
+        expect(true).toBe(false)
+    })
+
+    test('should go to a new page', async() => {
+        const results = await axiomApi.goto('https://wwww.axiom.ai', true, false)
+        expect(true).toBe(false)
+    })
+
+    test('should press keys', async() => {
+        const results = await axiomApi.pressKeys('Space', '|', 0)
+        expect(true).toBe(false)
+    })
+
+    test('should click and drag', async() => {
+        const results = await axiomApi.clickAndDrag({x: 0, y: 0}, {x: 100, y: 100})
+        expect(true).toBe(false)
+    })
+
+    test('should scrape metadata', async() => {
+        const results = await axiomApi.scrapeMetadata({})
+        expect(true).toBe(false)
+    })
+
+    test('should interact with select list', async() => {
+        const results = await axiomApi.selectList('.select', 'The Text')
+        expect(true).toBe(false)
+    })
+
+    test('should solve captcha', async() => {
+        const results = await axiomApi.solveCaptcha('apiKey')
+        expect(true).toBe(false)
+    })
+
+    test('should switch browser tab', async() => {
+        const results = await axiomApi.switchBrowserTab(1)
+        expect(true).toBe(false)
+    })
+
+    test('should wait', async() => {
+        const results = await axiomApi.wait({})
+        expect(true).toBe(false)
+    })
+
+    test('should hover', async() => {
+        const results = await axiomApi.hover('.button')
+        expect(true).toBe(false)
+    })
+
+    test('should restart browser', async() => {
+        const results = await axiomApi.restartBrowser()
+        expect(true).toBe(false)
     })
 
 });
